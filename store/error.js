55a6a7388b9ce2540307708f3a3cb019470b7421
HummingBird1224 <OksanaShuhaliia@gmail.com>
2023-06-01 04:21:30 +0200

    error store modify

--- store/error.js
+++ store/error.js
@@ -19,7 +19,7 @@ export const mutations = {
   },
 }
 
-export const actions = {
+export const actions = { //error actions
   async set({ commit }, { message , detail }) {
     commit('set', { message, detail })
     await this.$axios.post('/maintenance/error', { message, detail }).catch((err) => {console.log('Error!!!', err)})
